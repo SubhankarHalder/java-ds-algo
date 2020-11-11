0da87c86407d8f2cff2028a0c473648d86c4d10c
SubhankarHalder <subhankarvirgoan@gmail.com>
2020-11-11 09:17:31 +0530

    Add h-index file

---
+++ src/com/company/Hindex.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Hindex {
+    // The h-index is a metric that measures both the productivity
+    // and citation impact of a researcher.
+    // A researcher's h-index is the largest number h such that
+    // the researcher has published h papers that have
+    // each been cited at least h times.
+    //
+    // Ex: If Carl has publish A,B,C,D,E,F,G,H,I
+    // which have been cited 1,4,1,4,2,1,3,5,6 times respectively
+    // then his h-index is 4 (corresponding to papers B,D,H,I)
+    //
+    // Problem: Determine a researcher's h-index
+    // Input: Array of positive integers representing the citation
+    // counts of each of the author's papers.
+    public static void solution(int[] numbers){
+        Arrays.sort(numbers);
+        int cache = numbers[0];
+        int len = numbers.length;
+        for (int i = 0; i < len; i++){
+            // Code Here
+        }
+        System.out.println(numbers[0]);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // h-index solution
+        int[] test_case_1  = {1, 4, 1, 4, 2, 1, 3, 5, 6};
+        Hindex.solution(test_case_1);
+
     }
 }
